5276c45aaa27168775dcb49973fcd990e364589e
liberbell <liberbell@gmail.com>
2018-09-04 13:21:20 +0900

    view13

--- env3/web1/manager/views.py
+++ env3/web1/manager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
@@ -9,10 +9,12 @@ def index(request):
 
 def detail(request, question_id):
     # return HttpResponse("You're looking at question %s." % question_id)
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'manager/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'manager/detail.html', {'question': question})
 
 def results(request, question_id):
