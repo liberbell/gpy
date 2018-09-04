5499844dd6e03e13cd47b2eb77d0ce875778d1ac
liberbell <liberbell@gmail.com>
2018-09-04 09:11:26 +0900

    view11

--- env3/web1/manager/views.py
+++ env3/web1/manager/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
 
+# Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
