a789a0fa911d3145508db8d2cf09cb2dc9fe7965
liberbell <liberbell@gmail.com>
2018-09-04 09:08:10 +0900

    view8

--- env3/web1/manager/views.py
+++ env3/web1/manager/views.py
@@ -20,7 +20,7 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    template = loader.get_template('manager/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
