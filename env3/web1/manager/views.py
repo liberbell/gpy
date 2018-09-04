404e98bc048338347cddfe422319287061a4350f
liberbell <liberbell@gmail.com>
2018-09-04 09:06:20 +0900

    view6

--- env3/web1/manager/views.py
+++ env3/web1/manager/views.py
@@ -16,8 +16,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-    
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
