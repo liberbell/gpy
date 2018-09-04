b46795d4d302a989d87ec8f58f6f317cdc4e88d3
liberbell <liberbell@gmail.com>
2018-09-04 09:06:40 +0900

    view7

--- env3/web1/manager/views.py
+++ env3/web1/manager/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 def index(request):
