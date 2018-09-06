f622be9cf1b3c3dbb98234ffb049eb19e7b8f7ad
liberbell <liberbell@gmail.com>
2018-09-07 07:58:11 +0900

    admin7

--- env3/web1/manager/admin.py
+++ env3/web1/manager/admin.py
@@ -3,13 +3,17 @@ from .models import Choice, Question
 
 # Register your models here.
 # admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Title',            {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 
-admin.site.register(Choice)
+# admin.site.register(Choice)
