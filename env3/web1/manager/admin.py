7af87cb3a0f1e7c745fb0970ade19ebbc9955810
liberbell <liberbell@gmail.com>
2018-09-07 07:55:08 +0900

    admin6

--- env3/web1/manager/admin.py
+++ env3/web1/manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 # admin.site.register(Question)
@@ -11,3 +11,5 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
