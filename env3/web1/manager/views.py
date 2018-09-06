ca0bdc57d5b8e477443e990fcd6db7f9a0c9094e
liberbell <liberbell@gmail.com>
2018-09-06 09:08:07 +0900

    view16

--- env3/web1/manager/views.py
+++ env3/web1/manager/views.py
@@ -81,6 +81,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'manager/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
